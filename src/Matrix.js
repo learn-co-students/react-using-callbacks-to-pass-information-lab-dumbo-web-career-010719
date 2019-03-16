57aae8a83aaeb98f85bf7afa5199b3781f4632da
github username <github email address>
2019-03-16 13:18:28 -0400

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -5,12 +5,20 @@ import ColorSelector from './ColorSelector.js'
 
 export default class Matrix extends Component {
 
-  constructor() {
-    super()
+  state = {
+    color: "fff"
+  }
+  getColor=(data)=>{
+    // console.log(`matrix says hi: ${data}`)
+    this.state.color = data
+    console.log(`matrix ${this.state.color}`)
+  }
+  passColor=()=>{
+    return this.state.color
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} passColor={this.passColor}/>)
   )
 
   genMatrix = () => (
@@ -21,7 +29,7 @@ export default class Matrix extends Component {
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector getColor={this.getColor}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
